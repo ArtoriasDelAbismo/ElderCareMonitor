84ae7082df0da9ad8a0a7a2c9bb2fd4c38c87483
Jeronimo Journade <jerojournade1@gmail.com>
2025-11-17 06:57:20 -0300

    project re-structure/add high-low threshold bpm

---
+++ wear/src/main/java/com/example/eldercaremonitor/presentation/HearRateScreen.kt
@@ -0,0 +1,20 @@
+package com.example.eldercaremonitor.presentation
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.wear.compose.material.Text
+
+@Composable
+fun HeartRateScreen(hr: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = hr, textAlign = TextAlign.Center)
+    }
+}
