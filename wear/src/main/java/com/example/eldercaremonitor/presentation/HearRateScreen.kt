7376a733de57698c08eb73e6a40b41078b8c9003
Jeronimo Journade <jerojournade1@gmail.com>
2025-11-17 14:55:27 -0300

    add wearing state manager

--- wear/src/main/java/com/example/eldercaremonitor/presentation/HearRateScreen.kt
+++ wear/src/main/java/com/example/eldercaremonitor/presentation/HearRateScreen.kt
@@ -7,14 +7,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.Text
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HeartRateScreen(hr: String) {
+fun HeartRateScreen(hr: String, wearingStatus: String) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = hr, textAlign = TextAlign.Center)
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = wearingStatus, textAlign = TextAlign.Center)
     }
 }
--- wear/src/main/java/com/example/eldercaremonitor/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/eldercaremonitor/presentation/MainActivity.kt
@@ -11,11 +11,68 @@ import androidx.compose.runtime.*
 import androidx.core.content.ContextCompat
 import androidx.health.services.client.HealthServices
 import com.example.eldercaremonitor.presentation.theme.ElderCareMonitorTheme
-import com.example.eldercaremonitor.sensors.HeartRateMonitor
+import com.example.eldercaremonitor.sensors.HeartRateManager
+import com.example.eldercaremonitor.sensors.WearingStateManager
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import androidx.core.app.NotificationCompat
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var heartRateMonitor: HeartRateMonitor
+    private lateinit var heartRateManager: HeartRateManager
+    private lateinit var wearingManager: WearingStateManager
+
+
+    private fun vibrateWarning() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager =
+                getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            val vibrator = vibratorManager.defaultVibrator
+
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    600,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+
+        } else {
+            @Suppress("DEPRECATION")
+            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            @Suppress("DEPRECATION")
+            vibrator.vibrate(600)
+        }
+    }
+
+    private fun showWatchRemovedNotification() {
+        val manager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "wearing_alerts",
+                "Wearing Alerts",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            manager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, "wearing_alerts")
+            .setSmallIcon(android.R.drawable.stat_notify_error)
+
+            .setContentTitle("Watch Removed")
+            .setContentText("The device is no longer being worn.")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        manager.notify(1001, notification)
+    }
+    // -------------------------
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,24 +81,54 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             var hrText by remember { mutableStateOf("Waiting...") }
+            var wearingText by remember { mutableStateOf("Detecting") }
 
-            // Create monitor
-            heartRateMonitor = HeartRateMonitor(
+            // Heart Rate manager
+            heartRateManager = HeartRateManager(
                 measureClient = measureClient,
                 onHeartRateChanged = { bpm ->
                     hrText = "❤️ $bpm bpm"
                 },
-                onDangerousHeartRate = {bpm ->
+                onDangerousHeartRate = { bpm ->
                     hrText = "⚠️ DANGEROUS HR: $bpm bpm"
                 }
             )
+
+            // Wearing manager inside Compose
+            wearingManager = remember {
+
+            WearingStateManager(
+                    context = this@MainActivity,
+                    onWearingStateChanged = { isWearing ->
+
+                        wearingText = if (isWearing) {
+                            "Watch is being worn"
+                        } else {
+                            "⚠️ Watch removed!"
+                        }
+
+                        if (!isWearing) {
+                            vibrateWarning()
+                            showWatchRemovedNotification()
+                            heartRateManager.stop()
+                        } else {
+                            heartRateManager.start()
+                        }
+                    }
+                )
+            }
 
             val permissionLauncher =
                 rememberLauncherForActivityResult(
                     ActivityResultContracts.RequestPermission()
                 ) { granted ->
-                    if (granted) heartRateMonitor.start()
-                    else hrText = "Permission denied"
+                    if (granted) {
+                        heartRateManager.start()
+                        wearingManager.start()
+                    } else {
+                        hrText = "Permission denied"
+                        wearingText = "Permission denied"
+                    }
                 }
 
             LaunchedEffect(Unit) {
@@ -50,20 +137,25 @@ class MainActivity : ComponentActivity() {
                         Manifest.permission.BODY_SENSORS
                     ) == PackageManager.PERMISSION_GRANTED
                 ) {
-                    heartRateMonitor.start()
+                    heartRateManager.start()
+                    wearingManager.start()
                 } else {
                     permissionLauncher.launch(Manifest.permission.BODY_SENSORS)
                 }
             }
 
             ElderCareMonitorTheme {
-                HeartRateScreen(hr = hrText)
+                HeartRateScreen(
+                    hr = hrText,
+                    wearingStatus = wearingText
+                )
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        heartRateMonitor.stop()
+        heartRateManager.stop()
+        wearingManager.stop()
     }
 }
