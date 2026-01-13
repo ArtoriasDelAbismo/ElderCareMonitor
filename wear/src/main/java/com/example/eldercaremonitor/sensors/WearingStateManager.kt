d7e45b546263ec61d9e5c49555b006ffcc947f3c
Jeronimo <jerojournade1@gmail.com>
2026-01-13 16:08:24 -0300

    .

--- wear/src/main/java/com/example/eldercaremonitor/sensors/WearingStateManager.kt
+++ wear/src/main/java/com/example/eldercaremonitor/sensors/WearingStateManager.kt
@@ -22,8 +22,8 @@ class WearingStateManager(
     private var lastHeartRateTime: Long? = null
 
     private var isWorn = false
-    private val removalTimeout = 10_000L // 18 seconds
-    private val startupDelay = 5_000L    // Wait 5 seconds before assuming not worn
+    private val removalTimeout = 10_000L // 10 seconds
+    private val startupDelay = 10_000L    // Wait 5 seconds before assuming not worn
 
     private val removalChecker = object : Runnable {
         override fun run() {
